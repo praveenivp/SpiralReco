e312d7ca09c096f58c4482d7c5395210aae0233c
praveenivp <praveen.ivp@gmail.com>
2020-09-24 15:05:41 +0200

    added kspace folder

---
+++ kspace/GIRF_Correction.m
@@ -0,0 +1,198 @@
+% TASKS
+% 0. Rotate gradients interleaves(will do in GradtoK)
+% 1. calculate PSF with appropriate zeropadding(not required)
+% 2. apply PSF(done)
+% 3. Crossterm correction(done)
+% 4. give higher order kspace(done)
+% 5. verbose mode
+% 6. Rotation matrix/quatinions support
+% 7. write parameter parser and help text
+
+
+%time domain
+function [G_corr,parameters]=GIRF_Correction(G,PSF,varargin)
+
+%G - uncorrected gradient waveform
+    %G: (N x Axis x NIntlv) matrix correspond to each axis
+    %G and PSF should have same sampling time.
+ %PSF is in time domain
+    
+ %Parameters: Structure with following parameter
+ 
+ 
+ %% Input parameter check
+  switch(nargin)
+     case 1
+         error('need two input parameters Gradient[Samples x Axis x ...] and PSF [Samples x SPH x Axis] ');
+     case 2
+          warning('Using default Parmeters')
+          parameters.RotMat=eye(3);
+          parameters.isHigherOrderPSFCorr= false;
+          parameters.isCrossTermPSFCorr=false;
+          parameters.isB0Term=true;
+          parameters.verbose=false;
+          parameters.ConvType='same';
+          disp(parameters)
+     case 3
+         if(isstruct(varargin{1}))
+            parameters=varargin{1};
+         else
+            error('Third paramter is optional struct or Name-value pair') 
+         end
+      otherwise
+          p=inputParser;
+         addParameter(p,'RotMat',eye(3),@(x) ismatrix(x) && all(size(x,1)==[3,3]));
+         addParameter(p,'isHigherOrderPSFCorr',false,@(x) islogical(x));
+         addParameter(p,'isCrossTermPSFCorr',true,@(x) islogical(x));
+         addParameter(p,'isB0Term',true,@(x) islogical(x));
+         addParameter(p,'verbose',false,@(x) islogical(x));
+          %use 'full' for complete conv output or 'same' for truncated
+          %output
+         addParameter(p,'ConvType','same',@(x) any(validatestring(x,{'same','full'})));
+          parse(p,varargin{:});
+          parameters=p.Results;
+           
+  end
+          
+%%
+
+%make input gradient matrix into a 3D matrix
+ G=reshape(G,size(G,1),size(G,2),[]);
+ 
+  %Calculate physical gradients from XYZ gradient
+%   G_physical=parameters.rotmat*G;
+
+
+%Spherical harmonics selector:
+SPH_Sel=1:4; % B0,X,Y,Z by default
+
+if(parameters.isHigherOrderPSFCorr) 
+    SPH_Sel =1:size(PSF,2); 
+end
+% PSF=flip(PSF,1);
+ gc=zeros([size(G,1)+size(PSF,1)-1  max(SPH_Sel) size(G,3) 3]);
+for cintlv=1:size(G,3)
+    for axis=1:3
+     for sph=SPH_Sel
+         gc(:,sph,cintlv,axis)=conv(G(:,axis,cintlv),PSF(:,sph,axis));
+     end
+    end
+end
+
+%sanity check: Normally center/maximum position of all PSF function(self term) is the same 
+[~,idx]=max(PSF(:,[2 3+size(PSF,2) 4+size(PSF,2)*2]));
+if(sum(diff(idx))~=0)
+     warning('PSF has different Max idx: [%d %d %d]',idx(1),idx(2),idx(3)) 
+end
+
+idx(1)=idx(1)+1;
+%Different Convolution mode:
+if(strcmpi(parameters.ConvType,'same'))
+gc=gc(idx(1):idx(1)+size(G,1)-1,:,:,:);
+end
+
+
+%remote all cross term compenents when cross term effects are not required
+%Only removing Cross effects for X,Y,Z axis
+if(~parameters.isCrossTermPSFCorr)
+    gc(:,[3 4],:,1)=0;
+    gc(:,[2, 4],:,2)=0;
+    gc(:,[2, 3],:,3)=0;   
+end
+
+gc=sum(gc,4);
+
+if(parameters.verbose)
+figure,plot(gc(:,2:4,1)),hold on,plot(G(:,1:3))
+end
+
+
+if(parameters.isB0Term)
+    G_corr=gc;
+else
+    G_corr=gc(:,2:4,:);
+end
+
+end
+
+
+%Frequency domain
+function [G_corr,parameters]=GIRF_Correction_freq(G,PSF,parameters,verbose)
+
+%G - uncorrected gradient waveform
+    %G: (N x Axis x NIntlv) matrix correspond to each axis
+ %PSF is in time domain
+    
+ %Parameters: Structure with following parameter
+  parameters.rotmat=eye(3);
+  parameters.isHigherOrderPSFCorr= false;
+  parameters.isCrossTermPSFCorr=true;
+  parameters.isB0Term=true;
+  
+  %warning
+  if(size(G,1)>1500)
+   warn('PSF FT might be unstable')
+   %Need higher PSF of large time points or reduce input gradient length
+  end
+ 
+  %Calculate physical gradients from XYZ gradient
+%   G_physical=parameters.rotmat*G;
+  
+ZeroPadLength=800;%floor((4095-size(G,1))/2);  %800; % 8ms length of PSF measurement
+FFTLength= size(G,1)+ZeroPadLength;
+G_pad=padarray(G,[ZeroPadLength 0 0],0,'both');
+
+%   PSF_FT=fft(PSF,FFTLength,1);
+%   PSF_self=cat(2,PSF_FT(:,2,1),PSF_FT(:,3,2),PSF_FT(:,4,3));
+%   freq_axis=linspace(-0.5/10e-6,0.5/10e-6,size(PSF_self,1));
+%   pl(freq_axis,fftshift(PSF_self),[-50e3 50e3])
+
+
+% %calculate PSF
+load('D:\Data\GIRF_fieldcamera\20190718\GIRF_mat17082020_2','cal_sig','rec_sig')
+
+win_size=  FFTLength; 
+time_sel=1:800;
+in_sig=fft(cal_sig(time_sel,:,:,:,:,:),win_size,1);
+out_sig=fft(rec_sig(time_sel,:,:,:,:,:),win_size,1);
+ num=squeeze(sum(conj(in_sig).*out_sig,[2,4,5]));
+ denom=squeeze(sum(conj(in_sig).*in_sig,[2,4,5]));
+ PSF=num./denom;
+PSF(isnan(PSF))=0;
+PSF(isinf(PSF))=0;
+freq_axis=linspace(-1/1e-6,1/1e-6,size(PSF,1));
+ PSF_self=cat(2,PSF(:,2,1),PSF(:,3,2),PSF(:,4,3));
+  pl(freq_axis,fftshift(PSF_self),[-1.e6 1.e6])
+
+PSF_FT=PSF;
+
+
+G_FT=fft(G_pad,FFTLength,1);
+
+%Spherical harmonics selector:
+SPH_Sel=2:4; % X,Y,Z by default
+if(parameters.isB0Term) 
+    SPH_Sel = 1:4; 
+end
+if(parameters.isHigherOrderPSFCorr) 
+    SPH_Sel =1:size(PSF,2); 
+end
+
+G_corr=zeros([size(G,1) length(SPH_Sel)   size(G,3)]);
+for cintlv=1:size(G,3)
+
+GX_effects=ifft(bsxfun(@times,G_FT(:,1,cintlv),PSF_FT(:,SPH_Sel,1)),[],1);
+GY_effects=ifft(bsxfun(@times,G_FT(:,2,cintlv),PSF_FT(:,SPH_Sel,2)),[],1); 
+GZ_effects=ifft(bsxfun(@times,G_FT(:,3,cintlv),PSF_FT(:,SPH_Sel,3)),[],1);
+
+%remote all cross term compenents when cross term effects are not required
+if(~parameters.isCrossTermPSFCorr)
+    GX_effects(:,[2,3]+parameters.isB0Term)=0;
+    GY_effects(:,[1 3]+parameters.isB0Term)=0;
+    GZ_effects(:,[1, 2]+parameters.isB0Term)=0;   
+end
+%remove padding
+% G_corr(:,:,cintlv)=real(GX_effects(1:size(G,1),:)+GY_effects(1:size(G,1),:)+GZ_effects(1:size(G,1),:));
+G_corr(:,:,cintlv)=real(GX_effects(ZeroPadLength+(1:size(G,1)),:)+GY_effects(ZeroPadLength+(1:size(G,1)),:)+GZ_effects(ZeroPadLength+(1:size(G,1)),:));
+end
+end
---
+++ kspace/kspace.m
@@ -0,0 +1,187 @@
+classdef kspace
+    properties
+        Type;
+        Gradient % G/cm
+        G_dist % G/cm
+        k%kspace trajectory
+        k_dist % distorted traj
+        parameter=struct('Ninterleaves', 16,'Resolution',0.5,'FOV',[128 128 128 128],'MaxGradAmp',4,'MaxSlewrate',20,'DwellTime',5e-6)%fov,resolution
+        ZeroPadLength
+        %units FOV and Resolution are both in cm, maxGrad: G/cm,
+        %maxslew is G/cm/s
+        wi % density compensation
+        PSF
+        
+    end
+    methods
+        function obj= kspace(varargin) %  constructor
+            if(nargin==0)
+                obj.Type='SpiralOut';
+                obj.ZeroPadLength=512;
+                obj=obj.getk();
+                w=jacksonDCF2(obj.k,obj.parameter);
+                obj.wi=w./max(w(:));
+                obj=obj.calcPSF();
+                obj=distortk(obj);
+            elseif(nargin==1)
+                obj.parameter=varargin{1};
+                obj.Type='SpiralOut';
+                obj.ZeroPadLength=512;
+                obj=obj.getk();
+                w=jacksonDCF2(obj.k,obj.parameter);
+                obj.wi=w./max(w(:));
+                obj=obj.calcPSF();
+                obj=distortk(obj);
+            end
+        end
+        
+        function obj=getk(obj)
+            p=obj.parameter;
+            [~,G] = vdSpiralDesign(p.Ninterleaves, 4, p.Resolution*10,p.FOV,[0,0.2,0.3,1],p.MaxGradAmp,p.MaxSlewrate,p.DwellTime*1e3,[],'cubic');
+            %when resolution is 1cm, then fov becomes L cm
+            gammaH=4.2576e3; %Hz/G
+            
+            G=complex(G(:,1),G(:,2)); %G/cm
+            obj.Gradient=zeros(length(G)+2,p.Ninterleaves);
+            for intlv=1:p.Ninterleaves
+                obj.Gradient(:,intlv)=[0; G*exp(-1i*2*pi*(intlv/p.Ninterleaves)); 0;];
+            end
+            
+            %calculate kspace
+            obj.k= cumsum(2*pi*gammaH*obj.Gradient*p.DwellTime,1); % rad/cm
+            obj.k=obj.k./(2*pi)*(p.Resolution); %scale to  kmax=0.5
+            
+        end %getk
+        function obj=calcPSF(obj)
+            %Loads meaured triangular pulses to  
+            win_size=length(obj.Gradient)+obj.ZeroPadLength*2;
+            load('GIRF_0612_gammaH.mat','cal_sig','rec_sig');
+            in_sig=fft(cal_sig(:,:,:,:,:),win_size,1);
+            out_sig=fft(rec_sig(:,:,:,:,:),win_size,1);
+            num=squeeze(sum(sum(sum(conj(in_sig).*out_sig,2),4),5));
+            denom=squeeze(sum(sum(sum((abs(in_sig).^2),2),4),5));
+            obj.PSF=num./denom;
+            obj.PSF(isnan(obj.PSF) | isinf(obj.PSF))=0;
+        end
+        
+        function obj=distortk(obj)
+            win_size=length(obj.Gradient)+obj.ZeroPadLength*2;
+            gammaH=4.2576e3; %Hz/G
+            
+            
+            if (obj.ZeroPadLength>0)
+                gx=[zeros(obj.ZeroPadLength,obj.parameter.Ninterleaves); real(obj.Gradient); zeros(obj.ZeroPadLength,obj.parameter.Ninterleaves)];
+                gy=[zeros(obj.ZeroPadLength,obj.parameter.Ninterleaves); imag(obj.Gradient); zeros(obj.ZeroPadLength,obj.parameter.Ninterleaves)];
+            end
+            gxf=fft(gx,win_size,1);
+            gyf=fft(gy,win_size,1);
+            
+            gdist=complex(real(ifft(gxf.*obj.PSF(:,1))),real(ifft(gyf.*obj.PSF(:,2))));
+            
+            obj.G_dist=gdist(obj.ZeroPadLength+1:end-obj.ZeroPadLength,:);
+            %calculate kspace
+            obj.k_dist= cumsum(2*pi*gammaH*obj.G_dist*obj.parameter.DwellTime,1); % rad/cm
+            obj.k_dist=obj.k_dist./(2*pi)*(obj.parameter.Resolution); %scale to  kmax=0.5
+            
+        end
+        function obj=fixk(obj) 
+            % Dirty function: don't use
+            %replace the first and last few points of distorted Gradient
+            %from original gradient waveform
+            
+            gammaH=4.2576e3; %Hz/G
+            %             timeshift=0; %apparently after applying the PSF, gradients are shifted by 3 points
+%             obj.G_dist(1:end-timeshift+1,:)=obj.G_dist(timeshift:end,:);
+%             obj.G_dist(end-timeshift:end,:)=0;
+            
+            %replace first 10 and last ten values with orginal G
+            obj.G_dist(1:2,:)=obj.Gradient(1:2,:);
+            obj.G_dist(end-2:end,:)=obj.Gradient(end-2:end,:);
+            
+            %calculate kspace
+            obj.k_dist= cumsum(2*pi*gammaH*obj.G_dist*obj.parameter.DwellTime,1); % rad/cm
+            obj.k_dist=obj.k_dist./(2*pi)*(obj.parameter.Resolution); %scale to  kmax=0.5
+        end
+        
+        function obj = voronoidens(obj)
+            %
+            % function area = voronoidens(k);
+            %
+            % input:  k = kx + i ky is the  k-space trajectory
+            % output: area of cells for each point
+            %           (if point doesn't have neighbors the area is NaN
+           
+            kxy = [real(obj.k(:)),imag(obj.k(:))];
+            % returns vertices and cells of voronoi diagram
+            [V,C] = voronoin(kxy);
+            area = zeros(size(C));
+            for j = 1:length(kxy)
+                
+                try
+                    x = V(C{j},1);
+                    y = V(C{j},2);
+                    lxy = length(x);
+                    A = abs(sum( 0.5*(x([2:lxy 1]) - x(:)).*(y([2:lxy 1]) + y(:))));
+                catch
+                    warning('substituting area=0 for index %d',j);
+                    A=NaN;
+                end
+                area(j) =A;
+            end
+            area=reshape(area,size(obj.k));
+            
+            %remove last n% samples with samples:large area near the
+            %boundary
+            clip_point=round(0.9*size(obj.k,1));
+            area(clip_point:end,:)=repmat(area(clip_point,:),[length(clip_point:size(obj.k,1)) 1]);
+            
+            obj.wi =area./max(area(:)) ;
+        end
+        
+        function plotGradient(obj,intlv)
+            if(nargin<2)
+                intlv=1;
+            end
+            t=linspace(0,(size(obj.Gradient,1)-1)*10,size(obj.Gradient,1)); %us
+            %nominal gradients
+            gx=real(obj.Gradient(:,intlv))*10; % G/cm to mT/m
+            gy=imag(obj.Gradient(:,intlv))*10; % G/cm to mT/m
+            %distorted with PSF
+            gxr=real(obj.G_dist(:,intlv))*10; % G/cm to mT/m
+            gyr=imag(obj.G_dist(:,intlv))*10; % G/cm to mT/m
+            
+            figure,
+            plot(t,gxr,'color',	'#EDB120'),hold on,plot(t,gx,'color', 	'#D95319')
+            plot(t,gyr, 'LineStyle','--' ,   'color',	'#EDB120'),hold on,plot(t,gy,'LineStyle','--' ,  'color', 	'#D95319')
+            
+            legend('Reconstructed','Nominal'),xlabel('Time(us)'),ylabel('Grad Amplitude(mT/m)'),title('Gradients: GIRF results')
+            figure,
+            plot(t,(gx-gxr),'color',	'#EDB120'),hold on,plot(t,ones(size(gx)).*0.1),plot(t,ones(size(gx)).*-0.1),plot(t,zeros(size(t)))
+            plot(t,(gy-gyr), 'LineStyle','--' ,   'color',	'#EDB120')
+            legend('nominal-distorted','0.1mT/m','-0.1mT/m'),xlabel('time(us)'),ylabel('Deviations(mT/m)')
+        end
+        function plotKTraj(obj,intlv)
+            %plot the kspace trajectories of the interleave
+            if(nargin<2)
+                intlv=1;
+            end
+            t=linspace(0,(size(obj.Gradient,1)-1)*10,size(obj.Gradient,1)); %us
+            %nominal gradients
+            kx=real(obj.k(:,intlv));
+            ky=imag(obj.k(:,intlv));
+            %distorted with PSF
+            kxr=real(obj.k_dist(:,intlv));
+            kyr=imag(obj.k_dist(:,intlv));
+            
+            figure,
+            plot(t,kxr,'color',	'#EDB120'),hold on,plot(t,kx,'color', 	'#D95319')
+            plot(t,kyr, 'LineStyle','--' ,   'color',	'#EDB120'),hold on,plot(t,ky,'LineStyle','--' ,  'color', 	'#D95319')
+            
+            legend('Reconstructed','Nominal'),xlabel('Time(us)'),ylabel('Amplitude(Normalised)'),title('Kspace Trajectories: GIRF results')
+%             figure,
+%             plot(t,(kx-kxr),'color',	'#EDB120'),hold on,
+%             plot(t,(ky-kyr), 'LineStyle','--' ,   'color',	'#EDB120')
+%             legend('nominal-distorted'),xlabel('time(us)'),ylabel('Deviations(mT/m)')
+        end
+    end
+end
