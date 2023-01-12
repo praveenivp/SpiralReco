7d296d8448d9a49adc971f50f6d0dc28ffb1e270
praveenivp <praveen.ivp@gmail.com>
2023-01-12 14:18:13 +0100

    fixed double precion cgsense

--- PAT/spiralCGSENSE.m
+++ PAT/spiralCGSENSE.m
@@ -1,6 +1,6 @@
 function [img_cgsense,flag,relres,iter,resvec]=spiralCGSENSE(SOSOP,Datau,varargin)
 %  [img_cgsense,flag,relres,iter,resvec]=spiralCGSENSE(SOSOP,Datau,varargin)
-% 
+%
 % INPUTS:
 % StackofSpiral or StackofSpiralB0 operator
 % undersampled data: [ColxLinxParxCha]
@@ -37,20 +37,13 @@ switch(p.Results.reg)
         reg_out=zeros(SOSOP.imSize(SOSOP.imSize>0),'like',Datau);
         reg_out=reg_out(:);
 end
-        
 
-if(strcmp(SOSOP.precision,'double'))
-    if(ndims(SOSOP.op.sens)==3)
-        csm_sq = sum(SOSOP.op.sens.^2,[1,3]);
-    else
-        csm_sq = sum(SOSOP.op.sens.^2,4);
-    end
-    csm_sq(csm_sq < eps) = 1;
-    M=spdiags(double(csm_sq(:)), 0, numel(csm_sq), numel(csm_sq));
-    [img_cgsense,flag,relres,iter,resvec] = lsqr(E_FT, ([Datau(:); reg_out]),p.Results.tol,p.Results.maxit,M);
-    else
-    [img_cgsense,flag,relres,iter,resvec] = lsqr(E_FT, ([Datau(:); reg_out]),p.Results.tol,p.Results.maxit);
-end
+%    csm_sq = sum(SOSOP.op.sens.^2,[1,3]);
+%    csm_sq(csm_sq < eps) = 1;
+%     M=spdiags(double(csm_sq(:)), 0, numel(csm_sq), numel(csm_sq));
+%     [img_cgsense,flag,relres,iter,resvec] = lsqr(E_FT, ([Datau(:); reg_out]),p.Results.tol,p.Results.maxit,M);
+
+[img_cgsense,flag,relres,iter,resvec] = lsqr(E_FT, ([Datau(:); reg_out]),p.Results.tol,p.Results.maxit);
 img_cgsense=reshape(img_cgsense,SOSOP.imSize(1),SOSOP.imSize(2),[]);
 
 end
@@ -83,12 +76,12 @@ if (strcmp(transpose_indicator,'transp'))
        inpSize=[NUFFT_obj.dataSize,Ncha];
       outp = NUFFT_obj'*reshape(inp(1:prod(inpSize)),inpSize);
       if(numel(inp)>prod(inpSize)) %reg
-         outp=outp(:)+lambda.*double(inp((prod(inpSize)+1):end));
+         outp=outp(:)+lambda.*inp((prod(inpSize)+1):end);
       end
 elseif (strcmp(transpose_indicator, 'notransp'))
     inp=reshape(inp,NUFFT_obj.imSize(1),NUFFT_obj.imSize(2),[]);
-    reg=lambda.*double(inp(:));
-    outp = NUFFT_obj*double(inp);
+    reg=lambda.*inp(:);
+    outp = NUFFT_obj*inp;
     outp=[outp(:) ;reg];
 else
     error('Transpose flag not appropriately defined');
