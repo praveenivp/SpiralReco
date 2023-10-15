94a9853a6eff1664cf7a110394b66dfb1e5c5e91
praveenivp <praveen.ivp@gmail.com>
2023-10-15 18:25:08 +0200

    updated some demos

--- demo/RegularizedFieldmap_simulated.m
+++ demo/RegularizedFieldmap_simulated.m
@@ -6,17 +6,17 @@ MS=64; %matrix size
 b0map_true=(200/(2*pi*0.1))*exp(-0.5* (X.^2 + Y.^2)./(2*0.05))- ...
     (200/(2*pi*0.2))*exp(-0.5* ((X+0.2).^2 + (Y+0.1).^2)./(2*0.1))-...
     (300/(2*pi*0.2))*exp(-0.5* ((X-0.2).^2 + (Y-0.1).^2)./(2*0.1));
+b0map_true=b0map_true*4; %lot of warps
+figure,imagesc(b0map_true),colorbar,title(' field map [Hz]'),axis image
 
-figure,imagesc(b0map_true),colorbar,title(' field map(in Hz)');
-
-%% generate echoe images with complex noise
+%% generate multi-echo images with complex noise
 m = phantom('Modified Shepp-Logan',MS);
-T2star=m.*100e-3;
-TE=[2,3,7]*1e-3; %second
+T2star=m.*20e-3;
+TE=[2,4,6.5]*1e-3; %second
 L=length(TE);
 im_size=[MS MS];
 im=100*m.*... Proton density
-    exp(2i*pi*mod(bsxfun(@times,b0map_true,permute(TE,[3 1 2])),0.5)).*... Phase
+    exp(2i*pi*bsxfun(@times,b0map_true,permute(TE,[3 1 2]))).*... Phase
     exp(-1*(bsxfun(@rdivide,permute(TE,[3 1 2]) ,T2star)))... %relaxation effects
     +1e-3*complex(randn([MS MS length(TE)]),randn([MS MS length(TE)])); %noise
 im(isinf(im))=0;
@@ -25,17 +25,25 @@ figure,
 for i=1:L
 subplot(2,L,i)
 imagesc(abs(im(:,:,i)),[min(abs(im(:))) max(abs(im(:)))]),
-colorbar,title(sprintf('Magnitude image(TE=%d ms)',TE(i)*1e3))
+colorbar,title(sprintf('Magnitude image(TE=%d ms)',TE(i)*1e3)),axis image
 subplot(2,L,i+L)
 imagesc(angle(im(:,:,i))),
-colorbar,title(sprintf('Phase image(TE=%d ms)',TE(i)*1e3))
+colorbar,title(sprintf('Phase image(TE=%d ms)',TE(i)*1e3)),axis image
 end
 
 im=permute(im,[1 2 4 3]);
-%%
-[FieldMap,Fm_steps,g_steps]=RegularizedFieldMapEstimator(im,TE,[],1e-3,200);
+%% temporal unwarping and denoising with RegularizedFieldMapEstimator
+fm_initial=UMPIRE_unwrapp_3D(im,TE);
+[FieldMap,Fm_steps,g_steps]=RegularizedFieldMapEstimator(im,TE,fm_initial,1e-4,10e3);
  range=[min(b0map_true(:)) max(b0map_true(:))];
- figure,subplot(221),imagesc([],[],b0map_true,range),title('True value(Hz)'),colormap('parula'),colorbar
- subplot(222),imagesc([],[],Fm_steps(:,:,1)./(2*pi),range),title('Initial B0map(Hz)'),colormap('parula'),colorbar
- subplot(223),imagesc([],[],Fm_steps(:,:,end)./(2*pi),range),title('final B0map(Hz)'),colormap('parula'),colorbar
- subplot(224),imagesc([],[],Fm_steps(:,:,end)./(2*pi)-b0map_true,[-10 10]),title('final B0map-true(Hz)'),colormap('parula'),colorbar
+ % plot
+ figure,subplot(231),imagesc([],[],b0map_true,range),
+ title('True value [Hz]'),colormap('parula'),colorbar,axis image
+ subplot(232),imagesc([],[],fm_initial./(2*pi),range),
+ title('UMPIRE B0map [Hz]'),colormap('parula'),colorbar,axis image
+ subplot(233),imagesc([],[],Fm_steps(:,:,end)./(2*pi),range),
+ title('denoised B0map [Hz]'),colormap('parula'),colorbar,axis image
+  subplot(235),imagesc([],[],fm_initial./(2*pi)-b0map_true,[-10 10]),
+ title('UMPIRE B0map-true [Hz]'),colormap('parula'),colorbar,axis image
+ subplot(236),imagesc([],[],Fm_steps(:,:,end)./(2*pi)-b0map_true,[-10 10]),
+ title('denoised B0map-true [Hz]'),colormap('parula'),colorbar,axis image
--- demo/pseudo_replica_test.m
+++ demo/pseudo_replica_test.m
@@ -1,29 +1,40 @@
-%%
-cd('S:\Phantom\20220215_2dtest')
-r1=SpiralReco('meas_MID00559_FID08369_peSpiral_R1.dat','doDCF','voronoi');
+
+%% Create calib data, script for bart and registered fieldmap
+addpath(genpath('/ptmp/pvalsala/MATLAB'))
+Measpath='/ptmp/pvalsala/DKSR-UFYK';
+dir_st=dir(fullfile(Measpath,'TWIX','*peSpiral_R4*.dat'));
+clear commands;
+for i=1:(length(dir_st))
+    disp(dir_st(i).name)
+    [~,measID]=regexp(dir_st(i).name,'\S*#M(\d{2,}+)\S*','match','tokens');
+     csmfile=fullfile('/ptmp/pvalsala/DKSR-UFYK/SNR/dep_hansen',sprintf('fm_csm_MeasUID%d.mat',str2double(measID{1})));
+      load(csmfile,'csm','fm_interp')
+     SpObj=SpiralReco(fullfile(dir_st(i).folder,dir_st(i).name),'RepSel',2,...
+    'doPAT','CGSENSE','csm',csm,'maxit',10,'reg','Tikhonov','reg_lambda',1e-3,'compMode','CPU3D',...
+    'fm',-1*fm_interp,'doB0Corr','MTI','doDCF','Jackson','precision','double','NormNoiseData',false);
+
+
 R=[1 1];
 csm_chaLast=permute(csm,[2 3 4 1]);
-[sosop,sig]=undersampleSpiral(r1,csm_chaLast,R);
-im=sosop'*sig;
-as(im)
-%%
-R=[3 1];
-csm_chaLast=permute(csm,[2 3 4 1]);
-[sosop,sig]=undersampleSpiral(r1,csm_chaLast,R);
+[sosop,sig]=undersampleSpiral(SpObj,csm_chaLast,R);
 im_formation_fun=@(sig)  spiralCGSENSE(sosop,sig,...
-    'maxit',10,'tol',1e-6,'reg','none',...
-    'lambda',1e-2);
-im=im_formation_fun(sig);
-as(im./sqrt(prod(R)))
+    'maxit',10,'tol',1e-6,'reg','Tikhonov','lambda',1e-3);
+% im=im_formation_fun(permute(sig,[1 2 3 4]));
+% as(im./sqrt(prod(R)))
+   noise                = permute(SpObj.twix.noise(''),[1,3,4,5,6,2]);
+noise                = reshape(noise,[],size(noise,ndims(noise)));
+R                    = (noise.'*conj(noise))./(2*size(noise,1));
+R=R./mean(abs(diag(R)));
+noise_cov_matrix=0.5*(R+R.');
 
-
-
-%% pseudo replica
-[snr,g,noise_psf,img_noise_rep] = ismrm_pseudo_replica(sig*1e3, im_formation_fun, 50);
-as(snr)
+[snr,g,noise_psf,~,basline] = ismrm_pseudo_replica(sig, im_formation_fun, 128,noise_cov_matrix);
+Nrep=128;
+flags=SpObj.flags;
+    save(sprintf('M%d_%s.mat',str2double(measID{1}),SpObj.twix.hdr.Config.ProtocolName),'snr','g','noise_psf','basline','Nrep','flags')
+end
 
 %%
-function [sosop,sig,img_noise_rep]=undersampleSpiral(r1,csm,R)
+function [sosop,sig]=undersampleSpiral(r1,csm,R)
 
 sig=r1.sig(:,:,1:R(1):end,1:R(2):end,1);
 sig=permute(sig,[2 3 4 1]);
@@ -32,18 +43,22 @@ DCF=r1.DCF(:,1:R(1):end,1:R(2):end,1);
 
             kmax=2*pi*(0.5/(r1.SpiralPara.Resolution*1e-3));
 %             kmax=[kmax;kmax;(2*pi*0.5)/(1e-3*obj.SpiralPara.slice{1}.FOV_PRS(3)/obj.SpiralPara.NPartitions)];
-
-sosop=StackofSpirals(ktraj./(2*kmax),DCF,[r1.NUFFT_obj.imSize(1:2) 0],csm,...
-'CompMode','CPU2DHybrid','precision','single');
+sosop=r1.NUFFT_obj;
+% sosop=StackofSpirals(ktraj./(2*kmax),DCF,[r1.NUFFT_obj.imSize],csm,...
+% 'CompMode','CPU3D','precision','single');
 end
 
-function [snr,g,noise_psf] = ismrm_pseudo_replica(in, image_formation_func, reps)
+function [snr,g,noise_psf,img_noise_rep,baseline] = ismrm_pseudo_replica(in, image_formation_func, reps,Noise_cov)
+% from hansen
 baseline = image_formation_func(in);
 
 parfor r=1:reps,
     fprintf('Running pseudo replica %d/%d\n',r,reps);
-    n = complex(randn(size(in)),randn(size(in)));
-    s = in + n;
+    n_white = complex(randn(size(in)),randn(size(in)));    
+    n_color=chol(Noise_cov)*reshape(n_white,[],size(n_white,ndims(n_white))).';
+    n_color=reshape(n_color.',size(n_white));
+    
+    s = in + n_color;
     tmp = image_formation_func(s);
     img_noise_rep(:,r) = tmp(:);
 end
@@ -53,7 +68,7 @@ rep_dim = length(size(img_noise_rep));
 
 g = std(abs(img_noise_rep + max(abs(img_noise_rep(:)))),[],rep_dim); %Measure variation, but add offset to create "high snr condition"
 g(g < eps) = 1;
-snr = mean(img_noise_rep,3)./g;
+snr = mean(img_noise_rep,ndims(img_noise_rep))./g;
 
 img_noise_rep = img_noise_rep - repmat(baseline,[ones(1,length(size(baseline))) reps]);
 
